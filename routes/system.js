f07562a4ae1ef6640145353814c9ec11d248b115
PINK <henghengrahman@gmail.com>
2026-04-19 08:57:46 +0700

    Update system.js

--- routes/system.js
+++ routes/system.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
 const systemController = require('../controllers/systemController');
 
-router.get('/sitemap.xml', systemController.sitemap);
-router.get('/robots.txt', systemController.robots);
+router.get('/robots.txt', (req, res, next) => {
+  res.type('text/plain; charset=utf-8');
+  res.set('Cache-Control', 'public, max-age=3600, stale-while-revalidate=86400');
+  return systemController.robots(req, res, next);
+});
+
+router.get('/sitemap.xml', (req, res, next) => {
+  res.type('application/xml; charset=utf-8');
+  res.set('Cache-Control', 'public, max-age=3600, stale-while-revalidate=86400');
+  return systemController.sitemap(req, res, next);
+});
 
 module.exports = router;
