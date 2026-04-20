651fdf859fe7c6b9b4f66e171dfa7a8359663e2b
PINK <henghengrahman@gmail.com>
2026-04-20 09:56:22 +0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -9,25 +9,6 @@ const { makeMeta } = require('./helpers/seo');
 const { getCart, cartCount, cartTotals } = require('./helpers/cart');
 const { generateSeoPages } = require('./helpers/seo-pages');
 
-/* ================= CACHE SEO PAGES (ANTI BERAT) ================= */
-const SEO_PAGES = generateSeoPages();
-
-/* ================= HELPER SHUFFLE (AMAN) ================= */
-function getRandomLinks(arr, total = 50) {
-  const result = [];
-  const used = new Set();
-
-  while (result.length < total && result.length < arr.length) {
-    const i = Math.floor(Math.random() * arr.length);
-    if (!used.has(i)) {
-      used.add(i);
-      result.push(arr[i]);
-    }
-  }
-
-  return result;
-}
-
 function viewGlobals(req, res, next) {
   const settings = getSettings() || {};
   const cart = getCart(req);
@@ -87,12 +68,8 @@ function viewGlobals(req, res, next) {
     res.setHeader('X-Robots-Tag', res.locals.meta.robots);
   }
 
-  /* ================= 🔥 GLOBAL INTERNAL LINK (FIX AMAN) ================= */
-  try {
-    res.locals.internalLinks = getRandomLinks(SEO_PAGES, 50);
-  } catch (e) {
-    res.locals.internalLinks = [];
-  }
+  /* ================= 🔥 SEO LINKS (FIX ERROR REQUIRE) ================= */
+  res.locals.seoLinks = generateSeoPages().slice(0, 100);
 
   /* ================= STRUCTURED DATA ================= */
   res.locals.structuredData = null;
